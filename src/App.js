965e4740b2bc63fe1b0024d89673c5354eee0633
Julia <77528734+juliagronau@users.noreply.github.com>
2021-04-12 17:10:31 +0200

    Add HighScore Component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-import { useContext } from 'react';
-import { PokeContext } from './context/context';
+import "./App.css";
+import { useContext } from "react";
+import { PokeContext } from "./context/context";
+import { Switch, Route } from "react-router-dom";
+import HighScore from "./components/HighScore";
 
 function App() {
-  const {pokedex} = useContext(PokeContext)
+  const { pokedex } = useContext(PokeContext);
 
   return (
     <div className="App">
-      <img src="https://res.cloudinary.com/lmn/image/upload/c_limit,h_392,w_696/e_sharpen:100/f_auto,fl_lossy,q_auto/v1/gameskinnyc/a/6/0/a6078c-2fd9033b2be04c9b947d1aac6cdaef60-a2551.gif"/>
+      <img src="https://res.cloudinary.com/lmn/image/upload/c_limit,h_392,w_696/e_sharpen:100/f_auto,fl_lossy,q_auto/v1/gameskinnyc/a/6/0/a6078c-2fd9033b2be04c9b947d1aac6cdaef60-a2551.gif" />
 
       <h1>Gotta catch them all!!! </h1>
-      <ul>
-        {pokedex.map(pokemon => <li><strong>{pokemon.name}</strong>: HP:{pokemon.stats[0].base_stat} Attack:{pokemon.stats[1].base_stat} Defense: {pokemon.stats[2].base_stat} Id:{pokemon.id}<img src={pokemon.sprites.back_default}/></li>)}
-        </ul> 
-      
-
+      <Switch>
+        <Route exact path="/highscore">
+          <HighScore />
+        </Route>
+        <Route exact path="/">
+          <ul>
+            {pokedex.map(pokemon => (
+              <li>
+                <strong>{pokemon.name}</strong>: HP:{pokemon.stats[0].base_stat}{" "}
+                Attack:{pokemon.stats[1].base_stat} Defense:{" "}
+                {pokemon.stats[2].base_stat} Id:{pokemon.id}
+                <img src={pokemon.sprites.back_default} />
+              </li>
+            ))}
+          </ul>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import PokeState from './context/context'
-ReactDOM.render(
-  <React.StrictMode>
-    <PokeState> 
-    <App />
-    </PokeState>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import PokeState from "./context/context";
+import { BrowserRouter } from "react-router-dom";
+import { Grommet } from "grommet";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <Grommet>
+        <PokeState>
+          <App />
+        </PokeState>
+      </Grommet>
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
